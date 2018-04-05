e4fafd60748871d45577e96b6f3acb04a2b11a35
Sara Huang <37782974+luckicandigirl@users.noreply.github.com>
2018-04-04 23:15:47 -0400

    Delete poly1.m

--- poly1.m
+++
@@ -1,8 +0,0 @@
-function E=poly1(h,M)
-%
-% Performs type I polyphase decomposition of h in M components.
-% The ith row of E corresponds to the ith polyphase component.
-% Assumes that the first point of h is index 0.
-%
-h = [h zeros(1, ceil(length(h)/M)*M-length(h))];
-E = reshape(h, M, length(h)/M);
